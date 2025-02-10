575c9d889554993a4d72a48de3b994b328317b70
Carmine Paolino <carmine@paolino.me>
2025-02-10 20:26:28 +0100

    Add stream options and usage token handling in OpenAI provider

--- lib/ruby_llm/providers/openai.rb
+++ lib/ruby_llm/providers/openai.rb
@@ -43,6 +43,7 @@ module RubyLLM
             payload[:tools] = tools.map { |_, tool| tool_for(tool) }
             payload[:tool_choice] = 'auto'
           end
+          payload[:stream_options] = { include_usage: true } if stream
         end
       end
 
@@ -165,14 +166,16 @@ module RubyLLM
         end.compact
       end
 
-      def handle_stream(&block)
+      def handle_stream(&block) # rubocop:disable Metrics/MethodLength
         to_json_stream do |data|
           block.call(
             Chunk.new(
               role: :assistant,
               model_id: data['model'],
               content: data.dig('choices', 0, 'delta', 'content'),
-              tool_calls: parse_tool_calls(data.dig('choices', 0, 'delta', 'tool_calls'), parse_arguments: false)
+              tool_calls: parse_tool_calls(data.dig('choices', 0, 'delta', 'tool_calls'), parse_arguments: false),
+              input_tokens: data.dig('usage', 'prompt_tokens'),
+              output_tokens: data.dig('usage', 'completion_tokens')
             )
           )
         end
