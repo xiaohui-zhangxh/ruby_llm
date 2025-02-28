d18217bf0c5f21f4cb117d9a3664471bbcf13158
Carmine Paolino <carmine@paolino.me>
2025-02-28 12:27:14 +0100

    Fix base64 method naming in image generation specs for consistency

--- spec/ruby_llm/image_generation_spec.rb
+++ spec/ruby_llm/image_generation_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe RubyLLM::Image do
     it 'dall-e-3 can paint images' do # rubocop:disable RSpec/MultipleExpectations
       image = RubyLLM.paint('a siamese cat', model: 'dall-e-3')
 
-      expect(image.base_64?).to be(false)
+      expect(image.base64?).to be(false)
       expect(image.url).to start_with('https://')
       expect(image.mime_type).to include('image')
       expect(image.revised_prompt).to include('cat')
@@ -19,7 +19,7 @@ RSpec.describe RubyLLM::Image do
     it 'dall-e-3 supports custom sizes' do # rubocop:disable RSpec/MultipleExpectations
       image = RubyLLM.paint('a siamese cat', size: '1792x1024', model: 'dall-e-3')
 
-      expect(image.base_64?).to be(false)
+      expect(image.base64?).to be(false)
       expect(image.url).to start_with('https://')
       expect(image.mime_type).to include('image')
       expect(image.revised_prompt).to include('cat')
@@ -28,7 +28,7 @@ RSpec.describe RubyLLM::Image do
     it 'imagen-3.0-generate-002 can paint images' do # rubocop:disable RSpec/MultipleExpectations
       image = RubyLLM.paint('a siamese cat', model: 'imagen-3.0-generate-002')
 
-      expect(image.base_64?).to be(true)
+      expect(image.base64?).to be(true)
       expect(image.data).to be_present
       expect(image.mime_type).to include('image')
     end
